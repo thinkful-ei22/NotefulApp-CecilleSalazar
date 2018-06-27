e0a7abd508f6602650f040c7f9ecb61fb4da1679
Cecille Salazar <cecillersalazar@gmail.com>
2018-06-27 12:31:33 -0700

    Added and tested Delete to router

--- router/notes.router.js
+++ router/notes.router.js
@@ -52,6 +52,17 @@ router.post('/notes', (req, res, next) => {
   });
 });
 
+router.delete('/notes/:id', (req, res) => {
+  const { id } = req.params;
+
+  notes.delete(id, (err) => {
+    if (err) {
+      return next(err);
+    }
+    res.status(204).end();
+  });
+})
+
 router.get('/notes/:id', (req, res, next) => {
   const { id } = req.params;
 
