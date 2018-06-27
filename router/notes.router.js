7a0e466cc3439f2fe279d89d71a64334f4f2fd53
Cecille Salazar <cecillersalazar@gmail.com>
2018-06-27 12:20:15 -0700

    Created and tested POST in router.js

---
+++ router/notes.router.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const router = express.Router();
+const data = require('../db/notes');
+const simDB = require('../db/simDB');  // <<== add this
+const notes = simDB.initialize(data); // <<== and this
+
+router.put('/notes/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  /***** Never trust users - validate input *****/
+  const updateObj = {};
+  const updateFields = ['title', 'content'];
+
+  updateFields.forEach(field => {
+    if (field in req.body) {
+      updateObj[field] = req.body[field];
+    }
+  });
+
+  notes.update(id, updateObj, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.json(item);
+    } else {
+      next();
+    }
+  });
+});
+
+router.post('/notes', (req, res, next) => {
+  const { title, content } = req.body;
+
+  const newItem = { title, content };
+  /***** Never trust users - validate input *****/
+  if (!newItem.title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  notes.create(newItem, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+    } else {
+      next();
+    }
+  });
+});
+
+router.get('/notes/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  notes.find(id, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    res.json(item);
+  });
+});
+
+//No longer using testLogger - now using morgan
+//app.use(testLogger);
+//app.use(morgan('dev'))
+
+router.get('/notes', (req, res, next) => {
+  const { searchTerm } = req.query;
+
+  notes.filter(searchTerm, (err, list) => {
+    if (err) {
+      return next(err); // goes to error handler
+    }
+    res.json(list); // responds with filtered array
+  });
+});
+
+router.get('/boom', (req, res, next) => {
+  throw new Error('Boom!!');
+});
+
+module.exports = router;
