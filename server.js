f8bd828d6f9ac98f9900e9be375620cbf604c2dc
Cecille Salazar <cecillersalazar@gmail.com>
2018-06-29 11:37:54 -0700

    Added condition in server.js to prevent server from automatically starting  when tests are run

--- server.js
+++ server.js
@@ -99,8 +99,10 @@ app.use(function (err, req, res, next) {
   });
 });
 
-app.listen(PORT, function () {
-  console.info(`Server listening on ${this.address().port}`);
-}).on('error', err => {
-  console.error(err);
-});
+if (require.main === module) {
+  app.listen(PORT, function () {
+    console.info(`Server listening on ${this.address().port}`);
+  }).on('error', err => {
+    console.error(err);
+  });
+};
