fc73f6cf98e955182859db53a87ecc78bbf7080d
Cecille Salazar <cecillersalazar@gmail.com>
2018-06-29 14:01:06 -0700

    Added and Tested Conditions for GET-all notes

--- test/server.test.js
+++ test/server.test.js
@@ -29,7 +29,6 @@ describe('Express static', function () {
         expect(res).to.be.html;
       });
   });
-
 });
 
 describe('404 handler', function () {
@@ -41,5 +40,56 @@ describe('404 handler', function () {
         expect(res).to.have.status(404);
       });
   });
-
 });
+
+describe('GET request should return all notes', function() {
+  it ('should return 10 default notes as an array', function() {
+    return chai.request(app)
+      .get('/api/notes')
+      .then(function(res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('array');
+        expect(res.body.length).to.be.at.least(10);
+      });
+    });
+
+    it('should return a list with the correct right fields', function () {
+      return chai.request(app)
+        .get('/api/notes')
+        .then(function(res) {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('array');
+          expect(res.body).to.have.length(10);
+          res.body.forEach(function (item) {
+            expect(item).to.be.a('object');
+            expect(item).to.include.keys('id', 'title', 'content');
+          });
+        });
+    });
+
+    it('should return correct search results for a valid query', function() {
+      return chai.request(app)
+        .get('/api/notes?searchTerm=gaga')
+        .then(function(res) {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('array');
+          expect(res.body).to.have.length(1);
+          expect(res.body[0]).to.be.an('object');
+        });
+    });
+
+    it ('should return an empty array for an incorrect query', function() {
+      return chai.request(app)
+        .get('/api/notes?searchTerm=Hello%20Kitty')
+        .then(function(res) {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('array');
+          expect(res.body).to.have.length(0);
+        })
+    })
+
+  })
